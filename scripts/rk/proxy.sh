db9e73c1bb6d83cb5dfdb1e91d8fc43c74d76986
nikSm <nikitasmadych2001@gmail.com>
2024-11-04 08:01:59 +0000

    minor update

--- scripts/rk/proxy.sh
+++ scripts/rk/proxy.sh
@@ -1,4 +1,4 @@
-cd ../build/src
+cd ../../build/src
 ./gst-rtsp-launch --endpoint=khadas "( rtspsrc latency=0 buffer-mode=slave location=rtsp://192.168.143.25:8554/main.264 \
 ! queue max-size-buffers=0 \
 ! rtph265depay \
--- scripts/rk/udp-stream.sh
+++ scripts/rk/udp-stream.sh
@@ -5,5 +5,5 @@ gst-launch-1.0 rtspsrc latency=0 buffer-mode=slave location=rtsp://192.168.143.2
 ! h265parse config-interval=1 \
 ! queue max-size-buffers=0 \
 ! rtph265pay aggregate-mode=zero-latency mtu=1450 config-interval=1 name=pay0 pt=97 \
-! udpsink host=10.0.0.221 port=5600 sync=false async=false
-# ! udpsink host=192.168.144.20 port=5600 sync=false async=false
+! udpsink host=192.168.144.20 port=5600 auto-multicast=false sync=false async=false
+# ! udpsink host=10.0.0.221 port=5600 sync=false async=false
